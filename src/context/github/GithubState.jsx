0b6a1dee119560668938c3e2b7bfa2b9c693a250
Paolo Carcamo <Pogi@Paolo-Carcamo.local>
2020-09-02 15:32:05 -0700

    GithubState.jsx typo update

--- src/context/github/GithubState.jsx
+++ src/context/github/GithubState.jsx
@@ -59,7 +59,7 @@ const GithubState = (props) => {
   const getUserRepos = async (username) => {
     setLoading();
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${pgithubClientSecret}`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
     dispatch({
       type: GET_REPOS,
